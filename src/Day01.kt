def52122a82408accd729300fb526a8ba5b7c5ca
dchubarov <dime@twowls.org>
2023-12-01 17:24:38 +0400

    Solve Day01, single solution for both parts of the puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val digits = mapOf(
+        "1" to 1,
+        "2" to 2,
+        "3" to 3,
+        "4" to 4,
+        "5" to 5,
+        "6" to 6,
+        "7" to 7,
+        "8" to 8,
+        "9" to 9,
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    fun calibrationValue(s: String): Int {
+        val (_, l) = s.findAnyOf(digits.keys) ?: return 0
+        val (_, r) = s.findLastAnyOf(digits.keys, s.length)
+            ?: throw IllegalStateException()
+        return "${digits[l]}${digits[r]}".toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun calibrationSum(input: List<String>): Int =
+        input.map(::calibrationValue).sum()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = listOf(
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet"
+    )
+    check(calibrationSum(testInput) == 142)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val testInput2 = listOf(
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen"
+    )
+    check(calibrationSum(testInput2) == 281)
+
+    calibrationSum(readInput("Day01_test")).println()
 }
