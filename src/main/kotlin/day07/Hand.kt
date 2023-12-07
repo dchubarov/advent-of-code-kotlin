e40cd9d2d5c0a819946f3e7550e5e973081367b8
dchubarov <dime@twowls.org>
2023-12-07 22:59:33 +0400

    Implement day 7 (**)

---
+++ src/main/kotlin/day07/Hand.kt
@@ -0,0 +1,63 @@
+package day07
+
+val DEFAULT_DECK = setOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
+val JOKER_DECK = setOf('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
+
+class Hand(
+    private val covers: String,
+    deck: Set<Char> = DEFAULT_DECK,
+    wildcard: Char = Char.MIN_VALUE
+) : Comparable<Hand> {
+
+    private val strength: Int
+    private val cards: String
+
+    init {
+        require(covers.length == 5)
+        require(wildcard == Char.MIN_VALUE || wildcard in deck)
+        val frequencies = IntArray(deck.size)
+        val cardsBuilder = StringBuilder()
+        for (c in covers) {
+            deck.indexOf(c).let {
+                check(it >= 0)
+                frequencies[it]++
+                cardsBuilder.append(it.toString(16))
+            }
+        }
+
+        var maxStrength = calculateStrength(frequencies)
+        strength = if (wildcard == Char.MIN_VALUE)
+            maxStrength
+        else {
+            val wildcardIndex = deck.indexOf(wildcard)
+            if (frequencies[wildcardIndex] > 0) {
+                val wildcardFreq = frequencies[wildcardIndex]
+                frequencies[wildcardIndex] = 0
+
+                for (i in frequencies.indices) {
+                    if (frequencies[i] > 0) {
+                        frequencies[i] += wildcardFreq
+                        calculateStrength(frequencies).let {
+                            if (it > maxStrength)
+                                maxStrength = it
+                        }
+                        frequencies[i] -= wildcardFreq
+                    }
+                }
+            }
+            maxStrength
+        }
+
+        cards = cardsBuilder.toString()
+    }
+
+    private fun calculateStrength(a: IntArray) =
+        a.fold(initial = 0) { acc: Int, value: Int -> acc + value * value }
+
+    override fun compareTo(other: Hand): Int =
+        compareValuesBy(this, other, { it.strength }, { it.cards })
+
+    override fun toString(): String {
+        return "Hand{covers=$covers, strength=$strength}"
+    }
+}
---
+++ src/main/kotlin/day07/Solver.kt
@@ -0,0 +1,32 @@
+package day07
+
+import println
+import readInput
+
+fun totalWinnings(data: List<Pair<Hand, Int>>) =
+    data
+        .sortedBy { it.first }
+        .mapIndexed { index: Int, handAndBid: Pair<Hand, Int> -> handAndBid.second * (index + 1) }
+        .sum()
+
+fun main() {
+    totalWinnings(listOf("32T3K" to 765, "T55J5" to 684, "KK677" to 28, "KTJJT" to 220, "QQQJA" to 483)
+        .map { Hand(it.first, JOKER_DECK, 'J') to it.second }).println()
+
+    val fullInput = readInput("Day07")
+        .map {
+            it.split(" ").let { parts ->
+                parts[0] to parts[1].toInt()
+            }
+        }
+
+    // Part 01
+    totalWinnings(fullInput.map {
+        Hand(it.first) to it.second
+    }).println()
+
+    // Part 02
+    totalWinnings(fullInput.map {
+        Hand(it.first, JOKER_DECK, 'J') to it.second
+    }).println()
+}
