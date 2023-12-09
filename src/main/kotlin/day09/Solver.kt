5d2225b340d368ddc43c1d2c98a3373bad44b9a6
dchubarov <dime@twowls.org>
2023-12-09 14:13:54 +0400

    Implement day 9 (**)

---
+++ src/main/kotlin/day09/Solver.kt
@@ -0,0 +1,45 @@
+package day09
+
+import println
+import readInput
+
+fun Collection<Int>.extrapolateBothEnds(): Pair<Int, Int> {
+    if (isEmpty())
+        return 0 to 0
+
+    val stack = mutableListOf<Pair<Int, Int>>()
+    var diff = this
+    do {
+        var hasNonZero = false
+        diff = diff.zipWithNext().map {
+            val delta = it.second - it.first
+            if (delta != 0) {
+                hasNonZero = true
+            }
+            delta
+        }
+        if (hasNonZero) {
+            stack += diff.first() to diff.last()
+        }
+    } while (hasNonZero)
+
+    var lo = 0
+    var hi = 0
+    for (i in stack.indices.reversed()) {
+        lo = stack[i].first - lo
+        hi += stack[i].second
+    }
+
+    return (first() - lo) to (last() + hi)
+}
+
+fun main() {
+    check(listOf(0, 3, 6, 9, 12, 15).extrapolateBothEnds() == -3 to 18)
+    check(listOf(1, 3, 6, 10, 15, 21).extrapolateBothEnds() == 0 to 28)
+    check(listOf(10, 13, 16, 21, 30, 45).extrapolateBothEnds() == 5 to 68)
+
+    readInput("Day09")
+        .map { it.split(" ").filter(String::isNotBlank).map(String::toInt).extrapolateBothEnds() }
+        .reduce { acc, p -> (acc.first + p.first) to (acc.second + p.second) }
+        .println()
+}
